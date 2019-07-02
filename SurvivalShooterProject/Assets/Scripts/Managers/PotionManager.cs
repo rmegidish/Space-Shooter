d85e7291de195b2d5301a784e69f2257a20da201
rmegidish <rmegidis@uwo.ca>
2019-07-02 18:17:29 -0400

    Survival Shooter

---
+++ SurvivalShooterProject/Assets/Scripts/Managers/PotionManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionManager : MonoBehaviour {
+
+
+    public PlayerHealth playerHealth;
+    public GameObject player;
+    public GameObject Cylinder;  
+    public float spawnTime = 15f;
+    public Transform[] spawnPoints;
+   // public PlayerHealth potionClone;
+
+    private void Awake()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+       // potionClone = player.GetComponent<PlayerHealth>();
+       // Cylinder = GameObject.FindGameObjectWithTag("Cylinder");
+    }
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        
+    }
+   
+    void Spawn()
+    {
+
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+        if (playerHealth.currentHealth > 70f)
+        {
+            return;
+        }
+       
+
+
+        if (playerHealth.isCylinder == false){
+
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(Cylinder, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            playerHealth.isCylinder = true;
+            //  StartCoroutine(Wait(5f));
+            //  Cylinder.SetActive(false);
+        }
+    }
+/*   IEnumerator Wait(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+
+    } */
+
+}
---
+++ SurvivalShooterProject/Assets/Scripts/Player/PlayerPotion.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPotion : MonoBehaviour {
+
+  
+    public int extraHealth = 40;
+
+
+   
+    GameObject player;
+    PlayerHealth playerHealth;
+    GameObject Cylinder;
+    bool playerInRange;
+   // public PlayerHealth potionClone2;
+  
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        Cylinder = GameObject.FindGameObjectWithTag("Cylinder");
+       // potionClone2 = player.GetComponent<PlayerHealth>();
+
+    }
+
+  /*void Update()
+    {/*
+        if (playerHealth.currentHealth >=100){
+            Cylinder.SetActive(false);
+        } 
+} */
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+           
+            playerInRange = true;
+            playerHealth.currentHealth += extraHealth;
+            if (playerHealth.currentHealth > 100)
+            {
+                playerHealth.currentHealth = 100;
+            }
+            playerHealth.healthSlider.value = playerHealth.currentHealth;
+            Cylinder.SetActive(false);
+            playerHealth.isCylinder = false;
+        }
+      
+    }
+   /* private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player){
+            playerInRange = false;
+            Cylinder.SetActive(false);
+        }
+    }
+*/
+
+
+
+}
